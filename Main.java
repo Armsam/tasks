1b3fd58a6ba713489b3801760be10d23af51df59
Armsam <armensamvelyan96@gmail.com>
2021-04-26 18:05:30 +0400

    asdasd

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double radius=3;
+        double pi=3.14;
+        double area;
+        double perimete;
+        area=pi*radius*radius;
+        perimete= 2*pi*radius;
+        System.out.println("area=" + area);
+        System.out.println("perimete=" + perimete);
+
+
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Triangle {
+    public static void main(String[] args) {
+
+
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int area = a * b / 2;
+        int perimeter = a + b + c;
+        System.out.println("area=" + area);
+        System.out.println("perimeter=" + perimeter);
+    }
+}
